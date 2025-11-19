56383d98cbdeec05e0cbebee525f9965de2225b3
kae2604 <kae2604@ukr.net>
2025-11-19 21:34:23 +0200

    add hw2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const name = prompt('What is your name?');
+let answer= confirm(`${name} , Do you want to see a greeting`);
+if (answer) {
+    alert(`Hello, ${ name }! Welcome to our website!`);
+}
