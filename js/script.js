35b8b20e7ed318ddca03d6b8b1657956e944d321
kae2604 <kae2604@ukr.net>
2025-11-23 20:33:09 +0200

    add hw6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
 'use strict';
+
+let name = prompt('What is your name?');
+
+if (name === null || name === '') {
+    name = "You did not enter your name!";
+} else if( !isNaN(name)) {
+    name = "You enter your name incorrect!";
+}
+
+
+let age = prompt('What is your age?');
+
+if (age === null || age === '') {
+    age = "You did not enter your age!";
+} else if (age < 0 || isNaN(age)) {
+    age = "You entered incorrect data."
+}
+else {
+    age = +age;
+}
+
+
+let address = prompt('What is your address?');
+
+if (address === null || address === '') {
+    address = "You did not enter your address!";
+} else if( !isNaN(address) ) {
+    address = "You enter your address incorrect!";
+}
+
+
+let feelToJs = prompt('Do you like Java Script?')?.toLowerCase();
+
+if (feelToJs === 'yes') {
+    feelToJs = 'You like JavaScript!';
+} else if (feelToJs === 'no') {
+    feelToJs = 'You do not like JavaScript!';
+}  else if (feelToJs === undefined || feelToJs === '') {
+    feelToJs = 'You did not enter your opinion!';
+} else {
+    feelToJs = "You entered incorrect data."
+}
+
+alert(`Hello, ${name.charAt(0).toUpperCase() + name.slice(1)};
+Your age is: ${age};
+You live in: ${address.charAt(0).toUpperCase() + address.slice(1)};
+Attitude towards JavaScript: ${feelToJs}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
