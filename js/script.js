aea257d13e7275449e50c8d5a937ea01fd13cb49
kae2604 <kae2604@ukr.net>
2025-11-24 19:54:03 +0200

    change feelToJs hw6

--- js/script.js
+++ js/script.js
@@ -30,23 +30,41 @@ if (address === null || address === '') {
 }
 
 
+// In this code I compared strings:
+
+
+// let feelToJs = prompt('Do you like Java Script?')?.toLowerCase();
+//
+// if (feelToJs === 'yes') {
+//     feelToJs = 'You like JavaScript!';
+// } else if (feelToJs === 'no') {
+//     feelToJs = 'You do not like JavaScript!';
+// }  else if (feelToJs === undefined || feelToJs === '') {
+//     feelToJs = 'You did not enter your opinion!';
+// } else {
+//     feelToJs = "You entered incorrect data."
+// }
+
+
 let feelToJs = prompt('Do you like Java Script?')?.toLowerCase();
 
-if (feelToJs === 'yes') {
-    feelToJs = 'You like JavaScript!';
-} else if (feelToJs === 'no') {
-    feelToJs = 'You do not like JavaScript!';
-}  else if (feelToJs === undefined || feelToJs === '') {
+if (feelToJs === undefined || feelToJs === '') {
     feelToJs = 'You did not enter your opinion!';
 } else {
-    feelToJs = "You entered incorrect data."
+    feelToJs = feelToJs === 'yes';
+    if (feelToJs) {
+        feelToJs = 'You like JavaScript!';
+    }
+    else {
+        feelToJs = 'You do not like JavaScript or you enter incorrect data!';
+    }
 }
+
 
 alert(`Hello, ${name.charAt(0).toUpperCase() + name.slice(1)};
 Your age is: ${age};
 You live in: ${address.charAt(0).toUpperCase() + address.slice(1)};
 Attitude towards JavaScript: ${feelToJs}`);
-
 
 
 
