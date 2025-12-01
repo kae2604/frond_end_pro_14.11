3f691cd313173db270271fb9f8c733c632d995f6
kae2604 <kae2604@ukr.net>
2025-12-01 16:34:58 +0200

    change hw10

--- js/finalAccess.js
+++ js/finalAccess.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let userConditions = isAccess === "Access granted";
-let orderConditions = order.isPaid || balance > numberTotal;
+let orderConditions = order.isPaid || balance >= numberTotal;
 let systemConditions = fontSizeNumber > 12 && (systemSettings.language === "en" || systemSettings.language === "uk");
 let BlockConditions = null;
 let finalAccess = null;
