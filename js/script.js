e98173d26fb1bd19a5b03743d0b1bcdc9c11bd25
kae2604 <kae2604@ukr.net>
2026-01-09 11:22:56 +0200

    fix hw25

--- js/script.js
+++ js/script.js
@@ -8,102 +8,37 @@ ul.addEventListener('click', (event) => {
     if (removeBtn){
         liClicked.remove();
         return;
-    } else {
-        if (!liClicked) return;
-        const isSelected = liClicked.classList.contains('selected');
-        ul.querySelectorAll('.list_item').forEach(li => {
-            li.classList.remove('selected');
-        })
-        if (!isSelected){
-            liClicked.classList.add('selected');
-        }
+    }
+    if (!liClicked) return;
+    const isSelected = liClicked.classList.contains('selected');
+    ul.querySelectorAll('.list_item').forEach(li => {
+        li.classList.remove('selected');
+    })
+    if (!isSelected){
+        liClicked.classList.add('selected');
     }
 });
 
 const btn = document.querySelector('.button');
-// const li = document.querySelector('.list_item');
 
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     const input = document.querySelector('.input');
-    let textFromInput = input.value.trim();
+    const textFromInput = input.value.trim();
     if (!textFromInput){
         alert("Please enter something");
         return;
     }
     const liNew = document.createElement('li');
-
-
-
-
-
-    const textOfList = liNew.querySelector('.text_item');
-    textOfList.textContent = textFromInput;
-    input.value = "";
+    liNew.className = 'list_item';
+    const spanNew = document.createElement('span');
+    spanNew.className = 'text_item';
+    spanNew.textContent = textFromInput;
+    liNew.append(spanNew);
+    const buttonNew = document.createElement('button');
+    buttonNew.className = 'button_Li';
+    buttonNew.textContent = 'X';
+    liNew.append(buttonNew);
     ul.append(liNew);
+    input.value = "";
 })
-
-
-
-
-
-
-
-
-
-
-//
-// 'use strict';
-//
-// const ul = document.body.querySelector('.list');
-//
-// ul.addEventListener('click', (event) => {
-//     const liClicked = event.target.closest('.list_item');
-//     const removeBtn = event.target.closest('.button_Li');
-//     if (removeBtn){
-//         liClicked.remove();
-//     } else {
-//         if (!liClicked) return;
-//         const isSelected = liClicked.classList.contains('selected');
-//         ul.querySelectorAll('.list_item').forEach(li => {
-//             li.classList.remove('selected');
-//         })
-//         if (!isSelected){
-//             liClicked.classList.add('selected');
-//         }
-//     }
-// });
-//
-// const btn = document.querySelector('.button');
-// const li = document.querySelector('.list_item');
-//
-// btn.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     const input = document.querySelector('.input');
-//     let textFromInput = input.value.trim();
-//     if (!textFromInput){
-//         alert("Please enter something");
-//         return;
-//     }
-//     const liNew = li.cloneNode(true);
-//     const textOfList = liNew.querySelector('.text_item');
-//     textOfList.textContent = textFromInput;
-//     input.value = "";
-//     ul.append(liNew);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
