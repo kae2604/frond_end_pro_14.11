dc2c88c3c0ffc8c13de2d9576d639c61b38ccbfe
kae2604 <kae2604@ukr.net>
2026-01-09 10:47:33 +0200

    fix hw25

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ ul.addEventListener('click', (event) => {
     const removeBtn = event.target.closest('.button_Li');
     if (removeBtn){
         liClicked.remove();
+        return;
     } else {
         if (!liClicked) return;
         const isSelected = liClicked.classList.contains('selected');
@@ -20,7 +21,7 @@ ul.addEventListener('click', (event) => {
 });
 
 const btn = document.querySelector('.button');
-const li = document.querySelector('.list_item');
+// const li = document.querySelector('.list_item');
 
 btn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -30,7 +31,12 @@ btn.addEventListener('click', (event) => {
         alert("Please enter something");
         return;
     }
-    const liNew = li.cloneNode(true);
+    const liNew = document.createElement('li');
+
+
+
+
+
     const textOfList = liNew.querySelector('.text_item');
     textOfList.textContent = textFromInput;
     input.value = "";
@@ -46,8 +52,45 @@ btn.addEventListener('click', (event) => {
 
 
 
-
-
+//
+// 'use strict';
+//
+// const ul = document.body.querySelector('.list');
+//
+// ul.addEventListener('click', (event) => {
+//     const liClicked = event.target.closest('.list_item');
+//     const removeBtn = event.target.closest('.button_Li');
+//     if (removeBtn){
+//         liClicked.remove();
+//     } else {
+//         if (!liClicked) return;
+//         const isSelected = liClicked.classList.contains('selected');
+//         ul.querySelectorAll('.list_item').forEach(li => {
+//             li.classList.remove('selected');
+//         })
+//         if (!isSelected){
+//             liClicked.classList.add('selected');
+//         }
+//     }
+// });
+//
+// const btn = document.querySelector('.button');
+// const li = document.querySelector('.list_item');
+//
+// btn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const input = document.querySelector('.input');
+//     let textFromInput = input.value.trim();
+//     if (!textFromInput){
+//         alert("Please enter something");
+//         return;
+//     }
+//     const liNew = li.cloneNode(true);
+//     const textOfList = liNew.querySelector('.text_item');
+//     textOfList.textContent = textFromInput;
+//     input.value = "";
+//     ul.append(liNew);
+// })
 
 
 
