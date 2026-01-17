b760e210e49644fffa2bb4cde945b2170261ce5b
kae2604 <kae2604@ukr.net>
2026-01-17 22:40:10 +0200

    Add hw27

--- js/index.js
+++ js/index.js
@@ -8,36 +8,34 @@
         'fullName': /^(?=.{2,80}$)[\p{L}]+(?:[ '\-][\p{L}]+){0,3}$/u,
         'phone': /^\+[1-9]\d{7,14}$/,
         'address': /^(?=.{5,120}$)[\p{L}\d][\p{L}\d\s.,'’\-\/#]+$/u
-    }
+    };
 
     const errorMessages = {
         'fullName': 'Full Name Required',
         'phone': 'Phone Number Required',
         'address': 'Address Required',
-    }
-
-
+    };
 
     // General Variables
-    const toastAdded = new bootstrap.Toast(document.querySelector('#contactAdded'))
+    const toastAdded = new bootstrap.Toast(document.querySelector('#contactAdded'));
+    const toastRemoved = new bootstrap.Toast(document.querySelector('#contactRemoved'));
     const addContactModalSelector = '#addContactModal';
     const addContactModal = new bootstrap.Modal(addContactModalSelector, {
-        keyboard: false,
-        backdrop: 'static'
+        keyboard: true,
+        backdrop: true
     });
     const addContactModalTrigger = document.querySelector('[data-add-contact-modal-btn]');
 
     const addRemoveContactModalSelector = '#addRemoveContactModal';
     const addRemoveContactModal = new bootstrap.Modal(addRemoveContactModalSelector, {
-        keyboard: false,
-        backdrop: 'static'
+        keyboard: true,
+        backdrop: true
     });
 
     let IdContactToRemove = null;
 
     const contactsAlert = document.querySelector('[data-contacts-alert]');
     const contactsList = document.querySelector('[data-contacts-list]');
-
 
 
     // UI Handling
@@ -55,85 +53,69 @@
             btnRemoveContact.textContent = 'X';
             li.append(span, btnRemoveContact);
             return li;
-        }
+        };
 
         const addElement = (data) => {
-            const element = createItemTemplate(data)
-            contactsList.prepend(element)
+            const element = createItemTemplate(data);
+            contactsList.prepend(element);
             contactsList.classList.remove('d-none');
             contactsAlert.classList.add('d-none');
         }
 
         const removeElement = (id) => {
-            // Will be added later
-            const liForRemove = contactsList.querySelector(`[data-id-for-remove="${id}"]`)
-
+            const liForRemove = contactsList.querySelector(`[data-id-for-remove="${id}"]`);
             if (liForRemove) {
                 liForRemove.remove();
             }
-        }
+            if (contactsList.childElementCount === 0){
+                contactsAlert.classList.remove('d-none');
+            }
+        };
         return {
             addElement,
             removeElement,
-        }
-    }
-    const listHandler = uiContactsListHandler()
-
-
-
-
-
-
-
+        };
+    };
+    const listHandler = uiContactsListHandler();
 
     // State management
     const contactsManagement = () => {
         const contacts = [];
 
-
         const addContact = (data) => {
             const idForRemove = crypto.randomUUID();
             const contact = {idForRemove, ...data};
             contacts.push(contact);
-            console.log(contact)
-            console.log(contacts)
-        }
+        };
 
         const getContacts = () => {
             return structuredClone(contacts);
-        }
+        };
 
         const removeContact = (id) => {
-            // Will be developed later
             const IndexRemoveContact = contacts.findIndex(contact => {
                 return contact.idForRemove === id;
             });
-
             if (IndexRemoveContact === -1) {
-               return;
+               return false;
             }   else {
                 contacts.splice(IndexRemoveContact, 1);
+                return true;
             }
-            console.log(contacts)
-        }
-
+        };
         return {
             getContacts,
             addContact,
             removeContact
         }
-    }
+    };
     const contactService = contactsManagement();
-
 
 
     // Events
     addContactModalTrigger.addEventListener('click', () => {
         addContactModal.show()
-    })
-
-
-
+    });
 
     addContactModal._element.querySelector(`form#add-contact-form`)
         .addEventListener('submit', evt => {
@@ -153,21 +135,21 @@
                     formValidated = false;
                 }
                 return acc;
-            }, {})
+            }, {});
 
-            if(!formValidated) return null
+            if(!formValidated) return null;
 
             contactService.addContact(data);
-            const savedContact = contactService.getContacts().at(-1)
+            const savedContact = contactService.getContacts().at(-1);
             listHandler.addElement(savedContact);
-
             addContactModal.hide();
-            toastAdded.show()
+
+            const toastAddText = document.querySelector('[data-toast-add-body]');
+            toastAddText.textContent = `Contact "${savedContact.fullName}" successfully added!`;
+            toastAdded.show();
             evt.target.reset();
-            document.querySelectorAll('.error-validation').forEach(item => item.remove())
-
-        })
-
+            document.querySelectorAll('.error-validation').forEach(item => item.remove());
+        });
 
     contactsList.addEventListener('click', (event) => {
         const btnRemove = event.target.closest('.btnRemove-list-group-item');
@@ -179,19 +161,13 @@
         const spanForRemove = liForRemove.querySelector('span.span-list-group-item');
         const fullNameForRemove = spanForRemove.textContent.trim().split(' |')[0];
         const confirmRemoveContactTitle = addRemoveContactModal._element.querySelector('#staticBackdropLabelDeleteContact');
-        confirmRemoveContactTitle.textContent = `Are you sure you want to delete contact ${fullNameForRemove}?`;
+        confirmRemoveContactTitle.textContent = `Are you sure you want to delete contact "${fullNameForRemove}" ?`;
         IdContactToRemove = idForRemove;
-        addRemoveContactModal.show()
+        addRemoveContactModal.show();
     });
-
-
 
     document.querySelector('#addRemoveContactModal')
     .addEventListener('click', (event) => {
-
-
-            // addRemoveContactModal.hide();
-
         const btnConfirmRemove = event.target.closest('button');
         const CancelRemove = addRemoveContactModal._element.querySelector('[data-Remove-Cancel]');
         const ConfirmRemove = addRemoveContactModal._element.querySelector('[data-Remove-Confirm]');
@@ -200,21 +176,29 @@
             addRemoveContactModal.hide();
         }
         if (btnConfirmRemove === ConfirmRemove){
-        //     listHandler.removeElement(IdContactToRemove)
-        //     addRemoveContactModal.hide();
-        // IdContactToRemove = null;
-
+            const getNameContactToRemove = () => {
+                const findContactToRemove = contactService.getContacts().find(contact => {
+                    return contact.idForRemove === IdContactToRemove;
+                })
+                if (findContactToRemove){
+                    return findContactToRemove.fullName;
+                } else {
+                    return null;
+                }
+            };
+            const NameContactToRemove = getNameContactToRemove();
+            const toastRemovedText = document.querySelector('[data-toast-remove-body]');
+            toastRemovedText.textContent = `Contact "${NameContactToRemove }" successfully removed!`;
             contactService.removeContact(IdContactToRemove);
-
             listHandler.removeElement(IdContactToRemove);
-
-
+            toastRemoved.show();
             IdContactToRemove = null;
             addRemoveContactModal.hide();
 }
+    });
+})();
 
-    })
-})()
+
 // Global Scope
 
 
